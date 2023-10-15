d42f14ee1a13c1156777eb55ef8f459d6e5c2180
Jean 2P Principe <jean.pereyraprincipe@students.unibe.ch>
2023-10-15 16:44:04 +0200

    resetting the recommender system

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# DataMining_Recommender_system
+# Community-based Content Recommender System
+
+This repository contains a recommendation system that leverages the power of community detection algorithms to generate content suggestions. By analyzing community preferences from Twitch gamer data, the system provides insights and recommendations tailored to individual users.
+
+## Overview
+
+The main workflow of the project can be broken down into three parts:
+1. **Community Detection**: Using the `large_twitch_edges.csv` dataset, community detection algorithms (Louvain and Leiden) are applied to group similar users. The results of these algorithms are stored in separate CSV files for each algorithm.
+2. **Data Aggregation and Processing**: The main features of users, including their preferences, are loaded from `large_twitch_features.csv`. This data is then merged with the results from the community detection algorithms.
+3. **Recommendation Generation**: Recommendations are then generated for users based on either their community preferences alone or a hybrid of their personal and community preferences.
+
+## Important Files
+
+- **constants.py**: Contains the paths to various resource files and directories used throughout the project.
+- **large_twitch_edges.csv**: The main data file containing edges (interactions) between Twitch users.
+- **large_twitch_features.csv**: Contains features of individual Twitch users including their content preferences.
+- **louvain_community_assignments.csv**: Resulting community assignments from the Louvain algorithm.
+- **leiden_community_assignments.csv**: Resulting community assignments from the Leiden algorithm.
+- **main_communities.py**: The script responsible for applying community detection algorithms on user interactions and generating community assignment files.
+- **main_recommender_system.py**: The main script for generating content recommendations.
+
+## Execution Order
+
+1. Run `main_communities.py`. This script will apply community detection algorithms on the `large_twitch_edges.csv` dataset and generate `louvain_community_assignments.csv` and `leiden_community_assignments.csv`.
+2. Execute `main_recommender_system.py`. This script will:
+   - Load user features and merge them with community assignments.
+   - Calculate community preferences based on the `mature` feature.
+   - Generate content recommendations for a specified user.
+3. Examine the outputs. The merged data will be saved as `large_twitch_features_with_communities.csv` in the `outputs` directory.
+
+## Generated Files
+
+- **louvain_community_assignments.csv**: Contains user nodes and their respective community assignments from the Louvain algorithm.
+- **leiden_community_assignments.csv**: Contains user nodes and their respective community assignments from the Leiden algorithm.
+- **large_twitch_features_with_communities.csv**: Merged file containing user features along with their community labels from both the Louvain and Leiden algorithms.
+
+## Recommendations
+
+Upon executing `main_recommender_system.py`, content recommendations for a specified user ID will be printed out. These recommendations are based on community preferences, personal preferences, and a hybrid approach combining both.
+
+## Note
+
+Ensure all the paths in the `constants.py` file are correctly set up according to your system's directory structure. Also, make sure all necessary Python libraries are installed and that Python's path settings are appropriately configured for the project's environment.
--- src/main_recommender_system.py
+++ src/main_recommender_system.py
@@ -1,73 +1,132 @@
 import os.path
-
 import pandas as pd
-
-from constants import large_twitch_edges, large_twitch_features, outputs_path, louvain_file, leiden_file
-
-# Load the datasets into pandas dataframes
-edges_df = pd.read_csv(large_twitch_edges)
-features_df = pd.read_csv(large_twitch_features)
-
-# Read the pre-calculated community assignments
-louvain_communities = pd.read_csv(louvain_file)
-print(louvain_communities.columns)
-
-# Merge with the features_df dataframe
-features_df = pd.merge(features_df, louvain_communities, left_on='numeric_id', right_on='Node', how='left')
-
-# Now, 'features_df' will have a 'Community' column that contains community assignments
-features_df.rename(columns={'Community': 'community_label'}, inplace=True)
-
-filename = 'large_twitch_features_with_communities.csv'
-filename_path = os.path.join(outputs_path, filename)
-features_df.to_csv(filename_path, index=False)
-
-# Calculate the proportion of users preferring mature content in each community
-community_mature_preference = features_df.groupby('community_label')['mature'].mean()
-
-print(community_mature_preference)
+from constants import large_twitch_features, outputs_path, louvain_file, leiden_file
 
 
-# User-based Recommendations
-def recommend_content(user_numeric_id):
-    # Retrieve the community label associated with the provided user_numeric_id from the features dataframe
-    user_community = features_df.loc[features_df['numeric_id'] == user_numeric_id, 'community_label'].values[0]
+def load_data():
+    # Load the main features data and the community assignments from both algorithms
+    features_df = pd.read_csv(large_twitch_features)
+    louvain_communities = pd.read_csv(louvain_file)
+    leiden_communities = pd.read_csv(leiden_file)
 
-    # Check if the preference for mature content within the user's community is greater than the threshold (0.5 in
-    # this case)
-    if community_mature_preference[user_community] > 0.5:  # Change this threshold as per requirement
-        # If yes, recommend mature content to the user
-        return "[Community-based Algorithm] Recommend Mature Content"
+    # Merge Louvain community data
+    features_df = pd.merge(features_df, louvain_communities[['Node', 'Community']],
+                           left_on='numeric_id', right_on='Node', how='left')
+    features_df.drop(columns=['Node'], inplace=True)
+    features_df.rename(columns={'Community': 'community_label_louvain'}, inplace=True)
+
+    # Merge Leiden community data
+    features_df = pd.merge(features_df, leiden_communities[['Node', 'Community']],
+                           left_on='numeric_id', right_on='Node', how='left')
+    features_df.drop(columns=['Node'], inplace=True)
+    features_df.rename(columns={'Community': 'community_label_leiden'}, inplace=True)
+
+    # Save the merged data to a CSV file
+    filename_path = os.path.join(outputs_path, 'large_twitch_features_with_communities.csv')
+    features_df.to_csv(filename_path, index=False)
+
+    return features_df
+
+
+def get_algorithm_data(algorithm):
+    if algorithm == 'louvain':
+        return 'community_label_louvain', community_mature_preference_louvain
+    elif algorithm == 'leiden':
+        return 'community_label_leiden', community_mature_preference_leiden
     else:
-        # If no, recommend other types of content to the user
-        return "[Community-based Algorithm] Recommend Other Content"
+        raise ValueError("Invalid algorithm choice. Choose either 'louvain' or 'leiden'.")
 
 
-user_id_to_recommend = 123456  # Replace with the actual numeric_id
-recommendation = recommend_content(user_id_to_recommend)
-print(recommendation)
+def recommend_content(features_df, user_numeric_id, algorithm='louvain'):
+    """
+    Generate content recommendations based on the user and community preferences.
+
+    Parameters:
+    - features_df (pd.DataFrame): DataFrame containing user features and community labels.
+    - user_numeric_id (int): The numeric ID of the user to generate recommendations for.
+    - algorithm (str): The algorithm used for community detection ('louvain' or 'leiden').
+
+    Returns:
+    - str: A recommendation message.
+    """
+
+    # Get the appropriate community label column and mature preference based on the chosen algorithm
+    community_label_column, community_mature_preference = get_algorithm_data(algorithm)
+
+    # Retrieve the user data from the features dataframe using the provided user_numeric_id
+    user_data = features_df.loc[features_df['numeric_id'] == user_numeric_id]
+
+    # Extract the user's community label and mature preference value
+    user_community = user_data[community_label_column].values[0]
+    user_mature_pref = user_data['mature'].values[0]
+
+    # Extract the mature preference value for the community the user belongs to
+    community_pref = community_mature_preference[user_community]
+
+    # Generate recommendations based on the user's and community's mature content preference
+    if user_mature_pref == 1:
+        if community_pref > 0.5:
+            return f"[{algorithm.capitalize()}-based Algorithm] Recommend Mature Content"
+        else:
+            return f"[{algorithm.capitalize()}-based Algorithm] Recommend Other Content"
+    else:
+        if community_pref <= 0.5:
+            return f"[{algorithm.capitalize()}-based Algorithm] Recommend Other Content"
+        else:
+            return f"[{algorithm.capitalize()}-based Algorithm] Recommend Mature Content"
 
 
-#  Hybrid Strategy
-def hybrid_recommendation(user_numeric_id):
-    # Retrieve the community label for the given user_numeric_id from the features dataframe
-    user_community = features_df.loc[features_df['numeric_id'] == user_numeric_id, 'community_label'].values[0]
+def hybrid_recommendation(features_df, user_numeric_id, algorithm='louvain'):
+    """
+    Generate a hybrid content recommendation based on both individual user preferences and community preferences.
 
-    # Retrieve the 'mature' preference value for the given user_numeric_id
+    Parameters:
+    - features_df (pd.DataFrame): DataFrame containing user features and community labels.
+    - user_numeric_id (int): The numeric ID of the user to generate recommendations for.
+    - algorithm (str): The algorithm used for community detection ('louvain' or 'leiden').
+
+    Returns:
+    - str: A recommendation message based on the hybrid approach.
+    """
+
+    # Get the appropriate community label column and mature preference based on the chosen algorithm
+    community_label_column, community_mature_preference = get_algorithm_data(algorithm)
+
+    # Extract the user's community label and mature preference from the features dataframe
+    user_community = features_df.loc[features_df['numeric_id'] == user_numeric_id, community_label_column].values[0]
     user_mature_pref = features_df.loc[features_df['numeric_id'] == user_numeric_id, 'mature'].values[0]
 
-    # Check if both the user and the community predominantly prefer mature content
+    # If both user and community primarily prefer mature content, recommend mature content
     if user_mature_pref == 1 and community_mature_preference[user_community] > 0.5:
-        return "[Hybrid Algorithm] Recommend Mature Content"
-    # Check if both the user and the community predominantly prefer other content
+        return f"[{algorithm.capitalize()}-Hybrid Algorithm] Recommend Mature Content"
+
+    # If both user and community primarily prefer non-mature content, recommend non-mature content
     elif user_mature_pref == 0 and community_mature_preference[user_community] <= 0.5:
-        return "[Hybrid Algorithm] Recommend Other Content"
+        return f"[{algorithm.capitalize()}-Hybrid Algorithm] Recommend Other Content"
+
     else:
-        # This block handles the scenario where the user's individual preference and the community's preference do
-        # not align If they do not align, the system gives precedence to the individual's preference
-        return "[Hybrid Algorithm] Recommend Mature Content" if user_mature_pref == 1 else ("[Hybrid Algorithm] "
-                                                                                            "Recommend Other Content")
+        # If there's a disagreement between user's and community's preferences, prioritize the user's preference
+        # This ensures personalized recommendations even if the community's overall preference is different
+        return f"[{algorithm.capitalize()}-Hybrid Algorithm] Recommend Mature Content" if user_mature_pref == 1 else (
+            f"[{algorithm.capitalize()}-Hybrid Algorithm] Recommend Other Content")
 
 
-hybrid_recommendation = hybrid_recommendation(user_id_to_recommend)
-print(hybrid_recommendation)
+dataframe = load_data()
+
+# Calculate community preferences
+community_mature_preference_louvain = dataframe.groupby('community_label_louvain')['mature'].mean()
+community_mature_preference_leiden = dataframe.groupby('community_label_leiden')['mature'].mean()
+
+user_id_to_recommend = 123456
+
+recommendation_louvain = recommend_content(dataframe, user_id_to_recommend, 'louvain')
+print(recommendation_louvain)
+
+recommendation_leiden = recommend_content(dataframe, user_id_to_recommend, 'leiden')
+print(recommendation_leiden)
+
+hybrid_rec_louvain = hybrid_recommendation(dataframe, user_id_to_recommend, 'louvain')
+print(hybrid_rec_louvain)
+
+hybrid_rec_leiden = hybrid_recommendation(dataframe, user_id_to_recommend, 'leiden')
+print(hybrid_rec_leiden)
